7bffde908a7e827c948cf5838bf2eeb629b55530
Crowdbotics <team@crowdbotics.com>
2021-04-08 07:05:46 +0000

    Update models

--- backend/digital_card_25536/wsgi.py
+++ backend/digital_card_25536/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'digital_card_25536.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "digital_card_25536.settings")
 
 application = get_wsgi_application()
